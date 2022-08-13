f12d46010db980ea50336e3a0e105bc545724a42
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2022-08-13 23:55:54 +0900

    refact: format

--- voivoi/__main__.py
+++ voivoi/__main__.py
@@ -2,12 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """
     /
     """
     return {"message": "Hello World!"}
+
 
 if __name__ == '__main__':
     app.run()
