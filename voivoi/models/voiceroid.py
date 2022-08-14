2a8c9f57de289a7731b372ab0124f5e8028ccf62
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2022-08-14 17:44:56 +0900

    refact: black

--- voivoi/models/voiceroid.py
+++ voivoi/models/voiceroid.py
@@ -10,35 +10,31 @@ class Voiceroid:
         self.loadLanguage(language)
 
     def loadVoice(self, voice):
-        if (voice is not None):
-            if (voice in self.vc.listVoices()):
+        if voice is not None:
+            if voice in self.vc.listVoices():
                 self.vc.loadVoice(voice)
             else:
                 # NOTE: tts までいかなかった場合に明示的に object を破棄しないと
                 #       pyvcroid2 から ResultCode.ALREADY_INITIALIZED が raise される
                 del self.vc
-                raise exceptions.BadRequest(
-                    f"{voice} is not available in voice."
-                )
+                raise exceptions.BadRequest(f"{voice} is not available in voice.")
         else:
             self.vc.loadVoice(random.choice(self.vc.listVoices()))
 
     def loadLanguage(self, language):
-        if (language is not None):
-            if (language in self.vc.listLanguages()):
+        if language is not None:
+            if language in self.vc.listLanguages():
                 self.vc.loadLanguage(language)
             else:
                 # NOTE: tts までいかなかった場合に明示的に object を破棄しないと
                 #       pyvcroid2 から ResultCode.ALREADY_INITIALIZED が raise される
                 del self.vc
-                raise exceptions.BadRequest(
-                    f"{language} is not available in language."
-                )
+                raise exceptions.BadRequest(f"{language} is not available in language.")
         else:
             self.vc.loadLanguage(random.choice(self.vc.listLanguages()))
 
     def tts(self, text):
-        if (text is None):
+        if text is None:
             # NOTE: tts までいかなかった場合に明示的に object を破棄しないと
             #       pyvcroid2 から ResultCode.ALREADY_INITIALIZED が raise される
             del self.vc
